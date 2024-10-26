f758d820080fc07b5633772a9e9a7ff70e5ee18b
LesterCoronado <lestercoronado730@gmail.com>
2024-10-26 09:16:01 -0600

    a

--- src/db.js
+++ src/db.js
@@ -9,7 +9,6 @@ import {
 } from "./config.js";
 export const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
-  port: DB_PORT,
   dialect: DB_DIALECT,
   logging: false,
 });

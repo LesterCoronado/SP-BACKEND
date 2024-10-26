d2d2fde50e905d589da0c2588781267de497c605
LesterCoronado <lestercoronado730@gmail.com>
2024-10-26 09:23:16 -0600

    aja

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 export const PORT = process.env.PORT || 4000;
-export const DB_HOST = process.env.DB_HOST || "localhost";
+
+export const DB_HOST = process.env.DB_HOST || "mysql.railway.internal";
 export const DB_USER = process.env.DB_USER || "root";
-export const DB_PASSWORD = process.env.DB_PASSWORD || "123";
-export const DB_NAME = process.env.DB_NAME || "seminariodb";
-export const DB_PORT = process.env.DB_PORT || 3306;
+export const DB_PASSWORD = process.env.DB_PASSWORD || "eldWEAkZjqIkzCIvvDlwJawKHpkfjsIb";
+export const DB_NAME = process.env.DB_NAME || "railway";
+export const DB_PORT = process.env.DB_PORT || 26357;
 export const DB_DIALECT = process.env.DB_DIALECT || "mysql";
